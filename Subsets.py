f271e8a30312b5485cfa0f9b2167c192125fdc25
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2023-12-27 09:09:43 -0500

    Update Subsets.py with more BFS approachs

--- Subsets.py
+++ Subsets.py
@@ -42,3 +42,28 @@ class Solution:
                 queue.append(subset + [num])
 
         return queue
+
+
+    def subsets_fromJZSF(self, nums):
+        results = []
+
+        if not nums:
+            return results
+
+        nums.sort()
+
+        # BFS
+        queue = deque()
+        queue.append([])
+
+        while queue:
+            subset = queue.popleft()
+            results.append(subset)
+
+            for i in range(len(nums)):
+                if not subset or subset[-1] < nums[i]:
+                    nextSubset = list(subset)
+                    nextSubset.append(nums[i])
+                    queue.append(nextSubset)
+
+        return results
