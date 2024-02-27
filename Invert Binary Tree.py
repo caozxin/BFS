9be54c830b2bd6db5f0c8a86e92386cdec62db0f
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2024-02-26 22:13:51 -0500

    Update Invert Binary Tree.py
    even for in-place tree structure changes, the function should still return the input.

--- Invert Binary Tree.py
+++ Invert Binary Tree.py
@@ -1,1 +1,33 @@
 # Invert a binary tree.Left and right subtrees exchange.
+
+from lintcode import (
+    TreeNode,
+)
+
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: a TreeNode, the root of the binary tree
+    @return: nothing
+    note: this should be in place
+    """
+    def invert_binary_tree(self, root: TreeNode):
+        # write your code here
+        if not root:
+            return None
+
+        print(root.val)
+
+        left = self.invert_binary_tree(root.left)
+        right = self.invert_binary_tree(root.right)
+
+        root.left, root.right = right, left
+
+        return root
