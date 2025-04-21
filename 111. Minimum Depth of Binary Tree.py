d45c1bedd8465f9cea2f62f099f0e52defbfef5a
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2025-04-21 00:32:54 -0400

    Update 111. Minimum Depth of Binary Tree.py

--- 111. Minimum Depth of Binary Tree.py
+++ 111. Minimum Depth of Binary Tree.py
@@ -1,1 +1,35 @@
+from collections import deque
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        
+        #none handling:
+        if not root:
+            return 0 # this needs to be matching expected return format.
+
+        queue = deque([root])
+        res = []
+        level_idx = 0
+
+        while len(queue) > 0: # this is true as long as we have root.
+            # level = []
+            level_idx += 1
+            
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                val = node.val
+                # level.append(val)
+                print(val, level_idx)
+                # level_idx += 1
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+                if not node.left and not node.right:
+                    return level_idx
 
